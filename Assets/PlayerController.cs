ad400675325596a50594b183c3b4534b47afad69
CarolineClark <carolinejaneclark@gmail.com>
2017-07-03 11:00:31 +0100

    First commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	Rigidbody rb;
+	private float speed = 0.3f;
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+		float horizontal = Input.GetAxis("Horizontal") * speed;
+		float vertical = Input.GetAxis("Vertical") * speed;
+		rb.AddForce(new Vector3(horizontal, 0, vertical), ForceMode.Impulse);
+	}
+}
