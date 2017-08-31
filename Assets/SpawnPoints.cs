c389581f50c4a4eedf638ddf64edd8f49870d4ee
CarolineClark <carolinejaneclark@gmail.com>
2017-08-31 12:58:41 +0100

    Adds SpawnPoints

---
+++ Assets/SpawnPoints.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoints : MonoBehaviour {
+
+	private const string CASTLE = "Castle";
+	private const string BAKERY = "Bakery";
+	Transform castleSpawnPoint;
+	Transform bakerySpawnPoint;
+	public static SpawnPoints instance = null;
+
+	void Awake() {
+        if (instance == null) {
+            instance = this;
+        } else if (instance != this) {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+	}
+
+	void Start () {
+		castleSpawnPoint = transform.Find(CASTLE);
+		if (transformActive(castleSpawnPoint)) {
+			Debug.LogError("castle spawn point is not loaded");
+			UnityEditor.EditorApplication.isPlaying = false;
+		}
+		bakerySpawnPoint = transform.Find(BAKERY);
+		if (transformActive(bakerySpawnPoint)) {
+			Debug.LogError("bakery spawn point is not loaded");
+			UnityEditor.EditorApplication.isPlaying = false;
+		}
+	}
+
+	private bool transformActive(Transform t) {
+		return (t == null || !t.gameObject.activeSelf);
+	}
+	
+	void Update () {
+		
+	}
+}
